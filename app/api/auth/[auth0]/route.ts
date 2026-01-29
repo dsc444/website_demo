d19f317a7d941d5ed523017e060c570853b0459a
dsc444 <collinsdarragh9@gmail.com>
2026-01-29 23:54:57 +0000

    More auth0 problems

--- app/api/auth/[auth0]/route.ts
+++ app/api/auth/[auth0]/route.ts
@@ -1,3 +1,3 @@
-import { auth0 } from '@/lib/auth0'; // Adjust path to your auth0.ts file
+import { auth0 } from '@/app/lib/auth0'; // Adjust path to your auth0.ts file
 
 export const GET = auth0.middleware;
